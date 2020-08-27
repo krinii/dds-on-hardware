8054a67ababb3e7faa0b67661cb7b7be8e91ed9b
krinii <krini15@student.sdu.dk>
2020-08-27 15:13:54 +0200

    Started a looping program based on helloworld, loopoing not implemented yet

---
+++ dds_test/pubSubLoop/build/CMakeFiles/PublisherLoop.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "PubSubLoopData.c"
+  "PubSubLoopData.h"
+  "CMakeFiles/PublisherLoop.dir/publisher.c.o"
+  "CMakeFiles/PublisherLoop.dir/PubSubLoopData.c.o"
+  "PublisherLoop.pdb"
+  "PublisherLoop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PublisherLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dds_test/pubSubLoop/build/CMakeFiles/SubscriberLoop.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "PubSubLoopData.c"
+  "PubSubLoopData.h"
+  "CMakeFiles/SubscriberLoop.dir/subscriber.c.o"
+  "CMakeFiles/SubscriberLoop.dir/PubSubLoopData.c.o"
+  "SubscriberLoop.pdb"
+  "SubscriberLoop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SubscriberLoop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
