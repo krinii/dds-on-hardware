b0444df3b04185c408717985c9b6d57060950e48
krinii <krini15@student.sdu.dk>
2020-08-27 15:12:43 +0200

    Moved the example and changed the cmakelist to work from its new path

---
+++ dds_test/helloworld/build/CMakeFiles/HelloworldSubscriber.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "HelloWorldData.c"
+  "HelloWorldData.h"
+  "CMakeFiles/HelloworldSubscriber.dir/subscriber.c.o"
+  "CMakeFiles/HelloworldSubscriber.dir/HelloWorldData.c.o"
+  "HelloworldSubscriber.pdb"
+  "HelloworldSubscriber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloworldSubscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
