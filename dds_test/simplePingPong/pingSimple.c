6edb6daf4e3cf75fc497f6dee4ab9c772d14eae2
krinii <krini15@student.sdu.dk>
2020-09-07 17:47:13 +0200

    Added more comments

--- dds_test/simplePingPong/pingSimple.c
+++ dds_test/simplePingPong/pingSimple.c
@@ -11,6 +11,10 @@ dds_entity_t prepare_dds(dds_entity_t *dw, dds_entity_t *dr, dds_entity_t *topic
 dds_entity_t createParticipant(dds_entity_t *participantP);
 dds_entity_t createTopic(dds_entity_t *topicP);
 
+uint32_t discoverReader(dds_return_t *rc, dds_entity_t *dw);
+
+sPingPongData_Msg* readMsg(dds_return_t *rc, dds_entity_t *dr, sPingPongData_Msg *msg);
+
 int main (int argc, char ** argv)
 {
   dds_entity_t participant;
@@ -21,7 +25,6 @@ int main (int argc, char ** argv)
 
   sPingPongData_Msg msgO;
   sPingPongData_Msg *msg;
-  uint32_t status = 0;
   (void)argc;
   (void)argv;
 
@@ -29,33 +32,17 @@ int main (int argc, char ** argv)
   prepare_dds(&writer, &reader, &topic, &participant);
 
 
-
   printf("=== [Ping]  Waiting for a reader to be discovered ...\n");
   fflush (stdout);
 
-  /* Set status enabled on entity.This operation enables the status(es) based on the mask set */
-  /* dds_set _status_mask (entity, mask) 
-   * entity: Entity to enable the status. 
-   * mask: Status value that indicates the status to be enabled */
-  rc = dds_set_status_mask(writer, DDS_PUBLICATION_MATCHED_STATUS);
-  if (rc != DDS_RETCODE_OK)
-    DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-rc));
+  discoverReader(&rc, &writer);
 
-  // Wait for reader to appear on the network (what I understand so far 7/9 12:04)
-  while(!(status & DDS_PUBLICATION_MATCHED_STATUS))
-  {
-    rc = dds_get_status_changes (writer, &status);
-    if (rc != DDS_RETCODE_OK)
-      DDS_FATAL("dds_get_status_changes: %s\n", dds_strretcode(-rc));
-
-    /* Polling sleep. */
-    dds_sleepfor (DDS_MSECS (20));
-  }
 
   /* Create a message to write. */
   msgO.userID = 0;
   msgO.message = "Did you get the message, Pong";
 
+  /* To user: Print the information which is going to be sent */
   printf ("=== [Publisher]  Writing : ");
   printf ("Message (%"PRId32", %s)\n", msgO.userID, msgO.message);
   fflush (stdout);
@@ -64,40 +51,12 @@ int main (int argc, char ** argv)
   if (rc != DDS_RETCODE_OK)
     DDS_FATAL("dds_write: %s\n", dds_strretcode(-rc));
 
+  /* Wait for a message on the dataReader and then read it */
+  msg = readMsg(&rc, &reader, msg);
 
-  /* Prepare to read */
-  void *samples[MAX_SAMPLES];
-  dds_sample_info_t infos[MAX_SAMPLES];
-  /* Initialize sample buffer, by pointing the void pointer within
-   * the buffer array to a valid sample memory location. */
-  samples[0] = sPingPongData_Msg__alloc ();
-
-  /* Poll until data has been read. */
-  while (true)
-  {
-    /* Do the actual read.
-     * The return value contains the number of read samples. */
-    rc = dds_read (reader, samples, infos, MAX_SAMPLES, MAX_SAMPLES);
-    if (rc < 0)
-      DDS_FATAL("dds_read: %s\n", dds_strretcode(-rc));
-
-    /* Check if we read some data and it is valid. */
-    if ((rc > 0) && (infos[0].valid_data))
-    {
-      /* Print Message. */
-      msg = (sPingPongData_Msg*) samples[0];
-      printf ("=== [Subscriber] Received : ");
-      printf ("Message (%"PRId32", %s)\n", msg->userID, msg->message);
-      fflush (stdout);
-      break;
-    }
-    else
-    {
-      /* Polling sleep. */
-      dds_sleepfor (DDS_MSECS (20));
-    }
-  }
-
+  printf ("=== [Subscriber] Received : ");
+  printf ("Message (%"PRId32", %s)\n", msg->userID, msg->message);
+  fflush (stdout);
 
   /* Deleting the participant will delete all its children recursively as well. */
   rc = dds_delete (participant);
@@ -175,4 +134,73 @@ dds_entity_t prepare_dds(dds_entity_t *dw, dds_entity_t *dr, dds_entity_t *topic
   dds_delete_qos (drQos);
 
   return (*participant);
+}
+
+/**
+ * @brief Wait for a dataReader, matching the given dataWriter, to be dicovered.
+*/
+uint32_t discoverReader(dds_return_t *rc, dds_entity_t *dw){
+  uint32_t status = 0;
+  /* Set status enabled on entity.This operation enables the status(es) based on the mask set */
+  /* dds_set _status_mask (entity, mask) 
+   * entity: Entity to enable the status. 
+   * mask: Status value that indicates the status to be enabled */
+  // Make writer "listen" for the status
+  *rc = dds_set_status_mask(*dw, DDS_PUBLICATION_MATCHED_STATUS);
+  if (*rc != DDS_RETCODE_OK)
+    DDS_FATAL("dds_set_status_mask: %s\n", dds_strretcode(-*rc));
+
+  // Wait for a matching dataReader to appear on the network
+  while(!(status & DDS_PUBLICATION_MATCHED_STATUS))
+  {
+    *rc = dds_get_status_changes (*dw, &status);
+    if (*rc != DDS_RETCODE_OK)
+      DDS_FATAL("dds_get_status_changes: %s\n", dds_strretcode(-*rc));
+
+    /* Polling sleep. */
+    dds_sleepfor (DDS_MSECS (20));
+  }
+  return status;
+}
+
+/**
+ * @brief Read and return a message from the given dataReader.
+ * The function makes the required sample_info array and buffer array of pointers, their size is desided by MAX_SAMPLES. The buffer's pointer type has to be changed manually based on the IDL.
+ */
+sPingPongData_Msg* readMsg(dds_return_t *rc, dds_entity_t *dr, sPingPongData_Msg *msg){
+  /* Prepare to read */
+
+  void *samples[MAX_SAMPLES];
+  dds_sample_info_t infos[MAX_SAMPLES];
+  /* Initialize sample buffer, by pointing the void pointer within
+   * the buffer array to a valid sample memory location. */
+  samples[0] = sPingPongData_Msg__alloc ();
+
+  /* Poll until data has been read. */
+  while (true)
+  {
+    /* Do the actual read.
+     * The return value contains the number of read samples. */
+    *rc = dds_read (*dr, samples, infos, MAX_SAMPLES, MAX_SAMPLES);
+    if (*rc < 0)
+      DDS_FATAL("dds_read: %s\n", dds_strretcode(-*rc));
+
+    /* Check if we read some data and it is valid. */
+    if ((*rc > 0) && (infos[0].valid_data))
+    {
+      /* Print Message. */
+      msg = (sPingPongData_Msg*) samples[0];
+      // Prints has been moved out of function for easier re-use
+      /*printf ("=== [Subscriber] Received : ");
+      printf ("Message (%"PRId32", %s)\n", msg->userID, msg->message);
+      fflush (stdout);*/
+      break;
+    }
+    else
+    {
+      /* Polling sleep. */
+      dds_sleepfor (DDS_MSECS (20));
+    }
+  }
+  return msg;
 }
