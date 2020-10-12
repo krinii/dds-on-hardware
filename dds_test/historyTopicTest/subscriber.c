bc8c82d9622b00d574bd24ef97766de31c0a563a
krinii <krini15@student.sdu.dk>
2020-10-12 16:55:30 +0200

    Commented durability setting of topic

--- dds_test/historyTopicTest/subscriber.c
+++ dds_test/historyTopicTest/subscriber.c
@@ -45,7 +45,7 @@ int main (int argc, char ** argv)
 
   qosT = dds_create_qos ();
   dds_qset_history(qosT, DDS_HISTORY_KEEP_LAST, DEPTH);
-  dds_qset_durability(qosT, DDS_DURABILITY_TRANSIENT_LOCAL);
+  //dds_qset_durability(qosT, DDS_DURABILITY_TRANSIENT_LOCAL);
 
   /* Create a Topic. */
   /* dds_create_topic ( participant, descriptor, name, qos, listener ) */
